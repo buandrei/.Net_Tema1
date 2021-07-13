be83c72cc4f9f6fb5439fe6c64183f1ff394b3a6
buandrei <andreibu.ancora@gmail.com>
2021-07-13 15:08:56 +0300

    Add project files.

---
+++ Applicatie1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Applicatie1
+{
+    class Program
+    {
+        public static void Main()
+        {
+            var stringList = new string[] { "a", "ab", "abc", "abcd", "abcde" };
+
+            Console.WriteLine("Lista cu 5 elemente");
+            Console.WriteLine("Elementul cu cea mai mare lungime este " + FindMaximumLengthOfStringFromArray(stringList));
+            Console.WriteLine("Elementul cu cea mai mica lungime este " + FindMinimumLengthOfStringFromArray(stringList));
+
+            var longerStringList = new string[] { "a", "ab", "abc", "abcd", "abcde", "abcdef", "abcdefg", "abcdefgh", "abcdefghi", "abcdefghij" };
+
+            Console.WriteLine("\nLista cu 10 elemente");
+            Console.WriteLine("Elementul cu cea mai mare lungime este " + FindMaximumLengthOfStringFromArray(longerStringList));
+            Console.WriteLine("Elementul cu cea mai mica lungime este " + FindMinimumLengthOfStringFromArray(longerStringList));
+
+        }
+
+        public static string FindMaximumLengthOfStringFromArray(string[] list)
+        {
+
+            string longest = "";
+
+            foreach (var item in list)
+            {
+                if (longest.Length < item.Length)
+                    longest = item;
+            }
+            return longest;
+        }
+
+        public static string FindMinimumLengthOfStringFromArray(string[] list)
+        {
+
+            string shortest = "";
+
+            foreach (var item in list)
+            {
+                if (item != "")
+                {
+                    if (String.IsNullOrEmpty(shortest))
+                    {
+                        shortest = item;
+                    }
+                }
+
+                if (shortest.Length > item.Length)
+                {
+                    shortest = item;
+                }
+
+            }
+            return shortest;
+        }
+    }
+}
